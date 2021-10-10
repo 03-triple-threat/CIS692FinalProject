63c380414e01bd57ba418ab74c52ee6e3275c0be
AJ Hurtado <ajhurtado3@gmail.com>
2021-10-10 11:29:53 -0500

    backing up

--- app/src/main/java/com/example/cis_692_final_project/MainActivity.java
+++ app/src/main/java/com/example/cis_692_final_project/MainActivity.java
@@ -16,23 +16,30 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                 R.id.navigation_newEntry, R.id.navigation_newProgressPicture,
+//                R.id.navigation_history, R.id.navigation_settings)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+//    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                 R.id.navigation_newEntry, R.id.navigation_newProgressPicture,
-                R.id.navigation_history, R.id.navigation_settings)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+    protected void onCreate ( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+//        setContentView( R.layout.activity_main);
+        setContentView( R.layout.activity_new_entry);
     }
 
 }
