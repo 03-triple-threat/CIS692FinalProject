048154085af538687167d5d012c4b87137fdcc2a
AJ Hurtado <ajhurtado3@gmail.com>
2021-10-26 00:23:51 -0500

    backing up Settings screen

--- app/src/main/java/com/example/cis_692_final_project/MainActivity.java
+++ app/src/main/java/com/example/cis_692_final_project/MainActivity.java
@@ -39,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
 //        setContentView( R.layout.activity_main);
-        setContentView( R.layout.activity_new_entry);
+//        setContentView( R.layout.activity_new_entry);
 //        setContentView( R.layout.activity_history);
+        setContentView(R.layout.activity_settings);
     }
 
 }
